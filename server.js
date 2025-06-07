e9baf7e3259e59ec30de1ac6ce4510fcf9ce253a
killerchipy723 <diegoeduardo66@gmail.com>
2025-06-07 18:09:11 -0300

    bbb

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 
 let votos = { feliz: 0, bien: 0, triste: 0 };
 
